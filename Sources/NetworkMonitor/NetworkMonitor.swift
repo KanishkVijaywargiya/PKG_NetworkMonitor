72bf8e88ae1037c5d99e8adafd542bcb8ebb7c09
KanishkVijaywargiya <secretcodersociety@gmail.com>
2022-11-18 13:38:20 +0530

    First Commit

--- Sources/NetworkMonitor/NetworkMonitor.swift
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -1,6 +1,22 @@
-public struct NetworkMonitor {
-    public private(set) var text = "Hello, World!"
+import Network
+import Combine
 
+public class Network: ObservableObject {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "Monitor")
+
+    public var connected = false
+    
     public init() {
+        networkMonitor.pathUpdateHandler = { path in
+            self.connected = path.status == .satisfied
+            Task {
+                await MainActor.run {
+                    self.objectWillChange.send()
+                }
+            }
+        }
+        networkMonitor.start(queue: workerQueue)
     }
 }
+
